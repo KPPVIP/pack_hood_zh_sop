e9b3c601c1216608a17bfa7a3041f442faf7c21f
Guudz <Kilian1702@outlook.com>
2023-01-31 06:06:30 +0100

    Initial commit

---
+++ hoodf4l/__resource.lua
@@ -0,0 +1,22 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+this_is_a_map 'yes'
+
+files {
+	"test31_milo_.ymap",
+	'custom_timecycle_template.xml',
+	"lr_sc1_18.ymap",
+	"lr_sc1_18_critical_0.ymap",
+	"lr_sc1_18_long_0.ymap",
+	"lr_sc1_18_strm_0.ymap",
+	"lr_sc1_occl_00.ymap",
+	"lr_sc1_occl_03.ymap",
+	"lr_sc1_rd.ymap",
+	"lr_sc1_rd_long_0.ymap",
+	"lr_sc1_rd_strm_0.ymap",
+	"lr_sc1_rd_strm_2.ymap",
+	"lr_sc1_rd_strm_3.ymap",
+
+}
+
+data_file 'TIMECYCLEMOD_FILE' 'custom_timecycle_template.xml'
